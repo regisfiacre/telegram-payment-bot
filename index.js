e720d752074768245834b424076bf5c3fb39c9aa
regisfiacre <regissavy28@gmail.com>
2026-02-17 05:01:52 +0100

    Use environment variable for server port

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ app.post("/payment", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Bot running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Bot running on port " + process.env.PORT);
 });
